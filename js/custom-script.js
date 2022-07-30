0af585821be7971bb8bb6acfea5ce0eb3ef5f0ec
JATIN KUMAR VERMA <82764118+jatinkumarverma@users.noreply.github.com>
2022-07-30 10:52:41 +0530

    book style changed 🎉🎉

--- js/custom-script.js
+++ js/custom-script.js
@@ -108,3 +108,26 @@ function resetForm($form) {
   $form.find('input:radio, input:checkbox').removeAttr('checked').removeAttr('selected');
   $form.find('.form-control-feedback').css('display', 'none');
 }
+
+$(document).ready(function() {
+
+  // Pause just a moment
+  setTimeout(function() {
+
+    var $book = $('.book');
+
+    // Apply the intro classes that will 
+    // appear to turn the book,
+    // revealing its spine
+    $book.addClass('bookIntro');
+
+    // pause another moment, then turn back
+    setTimeout(function() {
+      $book.removeClass('bookIntro');
+    }, 2000);
+
+
+  }, 1000);
+
+
+});
